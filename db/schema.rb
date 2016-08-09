1e1b755a1572edbc9d4fa3af189c5a54ad832833
Robert President <bigmike73184@gmail.com>
2016-08-08 19:16:16 -0500

    Added Donations Table and Charities Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805010419) do
+ActiveRecord::Schema.define(version: 20160809000524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "charities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "donations", force: :cascade do |t|
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "amount_donated"
+    t.string   "charity_name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
