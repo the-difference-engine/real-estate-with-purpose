ab663188d48e2ce2f0f1955366f13f776f6d35c6
mjonesy <jonesy0007@gmail.com>
2016-09-08 16:20:13 -0500

    removed extra end in buyers controller and adjusted indentation

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,45 +1,44 @@
 class BuyersController < ApplicationController
-    def index
-      @buyers = Buyer.all
+  def index
+    @buyers = Buyer.all
+  end
+
+  def new
+    @buyer = Buyer.new
+  end
+
+  def create
+    @buyer = Buyer.create(name: params[:name])
+
+    flash[:success] = "New Buyer Created"
+    redirect_to '/buyers/#{@buyer.id}'
+  end
+
+  def show
+    @buyer = Buyer.find_by(id: params[:id])
+  end
+
+  def edit
+    @buyer = Buyer.find_by(id: params[:id])
+  end
+
+  def update
+    @buyer = Buyer.find_by(id: params[:id])
+
+    if @buyer.update(
+      name: params[:name]
+      )
     end
 
-    def new
-      @buyer = Buyer.new
-    end
+    flash[:success] = 'Buyer Updated'
+    redirect_to '/buyers/#{@buyer.id}'
+  end
 
-    def create
-      @buyer = Buyer.create(name: params[:name])
+  def destroy
+    @buyer = Buyer.find_by(id: params[:id])
+    @buyer.destroy
 
-      flash[:success] = "New Buyer Created"
-      redirect_to '/buyers/#{@buyer.id}'
-    end
-
-    def show
-      @buyer = Buyer.find_by(id: params[:id])
-    end
-
-    def edit
-      @buyer = Buyer.find_by(id: params[:id])
-    end
-
-    def update
-      @buyer = Buyer.find_by(id: params[:id])
-
-      if @buyer.update(
-        name: params[:name]
-        )
-      end
-
-      flash[:success] = 'Buyer Updated'
-      redirect_to '/buyers/#{@buyer.id}'
-    end
-
-    def destroy
-      @buyer = Buyer.find_by(id: params[:id])
-      @buyer.destroy
-
-      flash[:warning] = "Destroyed!"
-      redirect_to "/"
-    end
+    flash[:warning] = "Destroyed!"
+    redirect_to "/"
   end
 end
