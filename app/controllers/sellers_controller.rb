aaac60016300b8168dca600fef74cac5fd6284c5
mjonesy <jonesy0007@gmail.com>
2016-09-08 16:14:06 -0500

    removed extra end in sellers controller

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,42 +1,41 @@
 class SellersController < ApplicationController
-    def index
-      @sellers = Seller.all
+  def index
+    @sellers = Seller.all
+  end
+
+  def new
+    @seller = Seller.new
+  end
+
+  def create
+    @seller = Seller.create(name: params[:name])
+
+    flash[:success] = 'New Seller Created'
+    redirect_to '/sellers/#{@seller.id}'
+  end
+
+  def show
+    @seller = Seller.find_by(id: params[:id])
+  end
+
+  def edit
+    @seller = Seller.find_by(id: params[:id])
+  end
+
+  def update
+    @seller = Seller.find_by(id: params[:id])
+
+    if @seller.update(
+      name: params[:name]
+      )
     end
+  end
 
-    def new
-      @seller = Seller.new
-    end
+  def destroy
+    @seller = Seller.find_by(id: params[:id])
+    @seller.destroy
 
-    def create
-      @seller = Seller.create(name: params[:name])
-
-      flash[:success] = 'New Seller Created'
-      redirect_to '/sellers/#{@seller.id}'
-    end
-
-    def show
-      @seller = Seller.find_by(id: params[:id])
-    end
-
-    def edit
-      @seller = Seller.find_by(id: params[:id])
-    end
-
-    def update
-      @seller = Seller.find_by(id: params[:id])
-
-      if @seller.update(
-        name: params[:name]
-        )
-      end
-    end
-
-    def destroy
-      @seller = Seller.find_by(id: params[:id])
-      @seller.destroy
-
-      flash[:warning] = "Destroyed!"
-      redirect_to "/"
-    end
+    flash[:warning] = "Destroyed!"
+    redirect_to "/"
   end
 end
