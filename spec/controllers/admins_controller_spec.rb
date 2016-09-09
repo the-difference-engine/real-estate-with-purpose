2162306181d5512462f955de9dc8022e12e9b0a0
mjonesy <jonesy0007@gmail.com>
2016-09-08 20:36:41 -0500

    added index test

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AdminsController, type: :controller do
+  it 'routes get index' do
+    expect(:get => 'admins').to route_to(
+      :controller => 'admins',
+      :action => 'index')
+  end
 
 end
