672dfbc6bfef841b8bc8814e05751677fe47c78d
mjonesy <jonesy0007@gmail.com>
2016-09-08 19:04:37 -0500

    added admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 root to: 'home#index'
 
-get '/test' => 'pages#index'
+#admin routes
+get '/admins' => 'admins#index'
+get '/admins/new' => 'admins#new'
+post '/admins' => 'admins#create'
+get '/admins/:id' => 'admins#show' 
+get '/admins/:id/edit' => 'admins#edit'
+patch '/admins/:id' => 'admins#update'
+delete '/admins/:id' => 'admins#destroy'
 
 end
