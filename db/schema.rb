02f7ac52f3e0d5deb64dec7a07558307f097997a
kareningenuity <karenjxliu@hotmail.com>
2016-10-03 21:12:10 -0500

    added admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930004624) do
+ActiveRecord::Schema.define(version: 20161004011513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(version: 20160930004624) do
   create_table "donations", force: :cascade do |t|
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "amount_donated"
     t.string   "charity_name"
     t.integer  "charity_id"
-    t.float    "amount_donated"
   end
 
   create_table "properties", force: :cascade do |t|
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20160930004624) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
 end
