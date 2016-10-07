218d6b0c60987b6d8ba607b5e4c8a94f988de631
mjonesy <jonesy0007@gmail.com>
2016-10-06 20:44:26 -0500

    user routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -68,4 +68,7 @@ Rails.application.routes.draw do
   #user routes (for admins to view)
   get '/users' => 'users#index'
   get '/users/:id' => 'users#show'
+  get '/users/:id/edit' => 'users#edit'
+  patch '/users/:id' => 'users#update'
+  delete '/users/:id' => 'users#destroy'
 end
