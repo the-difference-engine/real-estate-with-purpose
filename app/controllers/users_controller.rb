d0a9058893c793bbb063d2f03c37e2c3560fa3eb
kareningenuity <karenjxliu@hotmail.com>
2016-10-19 18:37:47 -0500

    added sendgrid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
       password_confirmation: params[:password_confirmation]
       )
     if user.save
+      # Deliver the signup email
+      UserNotifier.send_signup_email(@user).deliver
       session[:user_id] = user.id
       flash[:success] = 'Successfully created account!'
       redirect_to '/'
