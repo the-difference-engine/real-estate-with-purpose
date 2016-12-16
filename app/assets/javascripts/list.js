cac8c717e4dd085b6546d663df96ac9367ef2ff1
kareningenuity <karenjxliu@hotmail.com>
2016-12-15 20:00:16 -0600

    modified javascript for listwithus page

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,4 +1,17 @@
 
+var current = "one"
+
+
+var steps = document.querySelectorAll(".step");
+steps.forEach(function(step){
+  step.addEventListener("click", changeStep)
+});
+
+function changeStep(input) {
+  document.getElementById(input).classList
+};
+
+
 var one = "<div id='step-description' class='step-content'><p>Step 1 description</p></div><div id='step-image' class='step-content'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/220px-Smiley.svg.png'></div>";
 
 var two = "<div id='step-description' class='step-content'><p>Step 2 description</p></div><div id='step-image' class='step-content'><img src='http://imgs.xkcd.com/comics/mysteries.png'></div>";
@@ -12,9 +25,3 @@ var five = "<div id='step-description' class='step-content'><p>Step 5 descriptio
 var six = "<div id='step-description' class='step-content'><p>Step 6 description</p></div><div id='step-image' class='step-content'><img src='http://www.nbc.com/the-tonight-show/content/sites/nbcutsjf/files/images/2014/10/06/ewalbumv0322.jpg'></div>";
 
 var donation = "<div id='step-description' class='step-content'><p> Donation description</p></div><div id='step-image' class='step-content'><img src='https://upload.wikimedia.org/wikipedia/en/8/84/Titanfall_box_art.jpg'></div>";
-
-
-
-function changeStep(id) {
- document.getElementById("content").innerHTML = id;
-}
