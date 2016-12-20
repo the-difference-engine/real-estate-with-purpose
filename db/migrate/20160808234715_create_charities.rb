07b539cc0ad4a9617fc0304e823f7ebb3cbe8a7a
risatino <charissatolentino@gmail.com>
2016-12-19 19:17:46 -0600

    Merging from qa

--- db/migrate/20160808234715_create_charities.rb
+++ db/migrate/20160808234715_create_charities.rb
@@ -7,5 +7,17 @@ class CreateCharities < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin, default: false
+      t.float :home_budget, default: 0.0
+
+      t.timestamps
+      
+    end
   end
 end
