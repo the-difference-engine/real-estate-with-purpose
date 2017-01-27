0d97295dc300a2c24aee270143e1c7ac564ec4fd
wabray17 <wabray17@gmail.com>
2017-01-26 20:47:55 -0600

    Added relationship table, created relation, started on user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :user_properties
+  has_many :properties, through: :user_properties
 
   def est_donation_amount
     self.home_budget * 0.025 * 0.7 * 0.1
---
+++ spec/factories/user_properties.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user_property do
+    user_id 1
+    property_id 1
+  end
+end
