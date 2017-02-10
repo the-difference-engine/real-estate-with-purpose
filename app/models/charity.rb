4a7b180fac4e1c779c9fc8a51600b6f7f086df1c
risatino <charissatolentino@gmail.com>
2017-02-09 21:02:19 -0600

    charities page needs edit, update, and destroy troubleshooting

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,3 +1,5 @@
 class Charity < ApplicationRecord
   has_many :donations
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 end
