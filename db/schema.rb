a0ff4163d48a3df479d703d2c4beb44516b2dc49
emplumb <emplumb@gmail.com>
2017-02-23 20:54:28 -0600

    Reruns db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170217022221) do
-=======
+
 ActiveRecord::Schema.define(version: 20170207012501) do
->>>>>>> ff4eb2172b7765f38b88e3e03c9792182638c2c7
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
