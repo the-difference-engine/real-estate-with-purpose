553a7890855e2949d2a90f25b1dae204884768ac
risatino <charissatolentino@gmail.com>
2017-04-25 22:09:18 -0500

    paperclip_dropbox bugs resolved :sparkles:

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,7 +2,7 @@ class Charity < ApplicationRecord
   has_many :donations
   do_not_validate_attachment_file_type :logo
   has_attached_file :logo, 
-    styles: { medium: "200x200#", thumb: "100x100#" }, 
+    styles: { medium: "200x200>", thumb: "100x100>" }, 
     :storage => :dropbox,
     :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
     :default_url => "/logos/:style/missing.png"
